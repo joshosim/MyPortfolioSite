b90e48eba5952353df6d59c06085cc1a03b7568a
osimuka <osimukaokpan@gmail.com>
2024-04-05 16:26:13 +0100

    okay

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,11 @@
 import React from "react";
 import UkaPhoto from "../images/uka.png";
 import OsimUkaPhoto from "../images/osimuka.jpg";
-import { FaBars, FaPhone } from "react-icons/fa";
+import { FaBars, FaLinkedin, FaPhone } from "react-icons/fa";
 import {
   FaFacebookSquare,
   FaInstagramSquare,
   FaTwitterSquare,
-  FaYoutubeSquare,
 } from "react-icons/fa";
 const HomePage = () => {
   return (
@@ -44,7 +43,7 @@ const HomePage = () => {
               Say Hi to <span className="text-pink-500">Osim Uka</span>
             </p>
             <h1 className="font-extrabold text-4xl mb-[1rem]">
-              <p>Your Full Stack</p>
+              <p>Your Front End</p>
               <p> Web Developer</p>
             </h1>
             <p className="mb-[1rem]">
@@ -152,19 +151,20 @@ const HomePage = () => {
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-[2rem] px-[1rem] md:px-[4rem]">
             <div className="rounded-3xl bg-white p-2">
               <div className="h-24 w-full rounded-3xl grid place-content-center bg-red-100">
-                <FaYoutubeSquare
-                  size={40}
-                  className="animate-bounce"
-                  color="red"
-                />
+                <FaLinkedin size={40} className="animate-bounce" color="blue" />
               </div>
               <div className="flex flex-col items-center justify-between gap-[1rem] mt-[1rem]">
                 <p className="font-bold text-2xl">
-                  867<span className="text-pink-500">K</span>
+                  1<span className="text-pink-500">K</span>
                 </p>
-                <p>Youtube Subscribers</p>
+                <p>Linkedin Connections</p>
                 <button className="rounded-3xl py-1 px-10 border-[1px] border-black font-bold hover:bg-black hover:text-white hover:duration-300 ease-in-out">
-                  Follow
+                  <a
+                    href="https://www.linkedin.com/in/uka-osim-9761601a0/"
+                    target="_blank"
+                  >
+                    Follow
+                  </a>
                 </button>
               </div>
             </div>
@@ -178,11 +178,16 @@ const HomePage = () => {
               </div>
               <div className="flex flex-col items-center justify-between gap-[1rem] mt-[1rem]">
                 <p className="font-bold text-2xl">
-                  867<span className="text-pink-500">K</span>
+                  3<span className="text-pink-500">K</span>
                 </p>
                 <p>Facebook Followers</p>
                 <button className="rounded-3xl py-1 px-10 border-[1px] border-black font-bold hover:bg-black hover:text-white hover:duration-300 ease-in-out">
-                  Follow
+                  <a
+                    href="https://www.facebook.com/uka.osim.56/"
+                    target="_blank"
+                  >
+                    Follow
+                  </a>
                 </button>
               </div>
             </div>
@@ -196,11 +201,13 @@ const HomePage = () => {
               </div>
               <div className="flex flex-col items-center justify-between gap-[1rem] mt-[1rem]">
                 <p className="font-bold text-2xl">
-                  867<span className="text-pink-500">K</span>
+                  1.5<span className="text-pink-500">K</span>
                 </p>
                 <p>Instagram Followers</p>
                 <button className="rounded-3xl py-1 px-10 border-[1px] border-black font-bold hover:bg-black hover:text-white hover:duration-300 ease-in-out">
-                  Follow
+                  <a href="https://www.instagram.com/ukaosim/" target="_blank">
+                    Follow
+                  </a>
                 </button>
               </div>
             </div>
@@ -214,7 +221,7 @@ const HomePage = () => {
               </div>
               <div className="flex flex-col items-center justify-between gap-[1rem] mt-[1rem]">
                 <p className="font-bold text-2xl">
-                  867<span className="text-pink-500">K</span>
+                  1<span className="text-pink-500">K</span>
                 </p>
                 <p>Twitter Followers</p>
                 <button className="rounded-3xl py-1 px-10 border-[1px] border-black font-bold hover:bg-black hover:text-white hover:duration-300 ease-in-out">
@@ -292,7 +299,7 @@ const HomePage = () => {
         {/* products */}
         <div className="px-[1rem] md:px-[5rem] py-[4rem]">
           <h1 className="text-center font-extrabold text-4xl mb-[2rem]">
-            My Products
+            My Projects
           </h1>
           <div className="flex flex-col md:flex-row items-center justify-between">
             <div>
