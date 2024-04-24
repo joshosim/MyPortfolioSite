2e1326d5e72ff522394855bbf230b881149ef1a8
osimuka <osimukaokpan@gmail.com>
2024-04-24 03:38:58 +0100

    sub

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,17 +4,10 @@ import OsimUkaPhoto from "../images/osimuka.jpg";
 
 // import { Link } from "react-router-dom";
 import {
-  FaBars,
   FaLinkedin,
-  FaPhone,
   FaFacebookSquare,
   FaInstagramSquare,
   FaTwitterSquare,
-  FaJs,
-  FaHtml5,
-  FaReact,
-  FaCss3,
-  FaGithub,
 } from "react-icons/fa";
 const HomePage = () => {
   const [navState, setNavState] = useState(true);
@@ -39,13 +32,40 @@ const HomePage = () => {
               Projects
             </li>
           </ul>
-          <FaBars size={20} className="block md:hidden" onClick={toggle} />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 block md:hidden"
+            onClick={toggle}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
           <button className="hidden md:block border-2 border-black py-2 px-6 font-semibold text-black rounded-3xl hover:bg-black hover:duration-300 hover:text-white">
             <a href="tel: +2347066530998">Contact Me</a>
           </button>
-          <button className="md:hidden border-2 border-black py-2 px-6 font-semibold text-black rounded-3xl">
+          <button className="md:hidden border-2 border-black p-2 rounded-full font-semibold text-black">
             <a href="tel: +2347066530998">
-              <FaPhone size={20} />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
+                />
+              </svg>
             </a>
           </button>
         </div>
@@ -84,8 +104,11 @@ const HomePage = () => {
               Creating and building Full Stack Web Applications for all
               companies.
             </p>
-            <button className="bg-black text-white rounded-3xl px-5 py-2 font-bold mb-[3rem] hover:scale-105">
+            <button className="bg-black text-white rounded-2xl px-5 py-3 font-bold mb-[.5rem] hover:scale-105 w-[200px]">
               Hire Me
+            </button>
+            <button className="bg-transparent border-2 border-white text-black rounded-2xl px-5 py-3 font-bold mb-[3rem] hover:scale-105 w-[200px] hover:bg-slate-100">
+              Download Resume
             </button>
             <div className="flex items-center justify-between">
               <div>
@@ -106,13 +129,7 @@ const HomePage = () => {
             <img src={OsimUkaPhoto} alt={UkaPhoto} className="object-cover" />
           </div>
         </div>
-        <div className="bg-black h-14 flex justify-between items-center py-4 px-6">
-          <FaJs color="yellow" size={35} />
-          <FaHtml5 color="orange" size={35} />
-          <FaReact color="darkBlue" size={35} />
-          <FaCss3 color="yellow" size={35} />
-          <FaGithub color="white" size={35} />
-        </div>
+
         {/* aboutme */}
         <div className="bg-white py-[4rem] px-[1rem] md:px-[5rem]">
           <h1 className="text-center font-extrabold text-4xl mb-[2rem]">
