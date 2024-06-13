d7d03e37b10b8e24b92be76ce72285b9b112a0f6
osimuka <osimukaokpan@gmail.com>
2024-06-14 00:32:51 +0100

    update

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -10,24 +10,24 @@ const Social = () => {
                 <p> Media Channels.</p>
             </h1>
             <div className='grid place-items-center'>
-                <div className="grid md:grid-cols-2 gap-4 px-[1rem]">
-                    <div className="rounded-3xl bg-white p-2 w-[230px]">
-                        <div className="h-24 w-full rounded-3xl grid place-content-center bg-red-100">
-                            <FaLinkedin size={40} color="blue" />
-                        </div>
-                        <div className="flex flex-col items-center justify-between gap-[1rem] mt-[1rem]">
-                            <p className="font-bold text-2xl">
-                                126<span className="text-pink-500"></span>
-                            </p>
-                            <p>Linkedin Connections</p>
-                            <button className="rounded-3xl py-1 px-10 border-[1px] border-black font-bold hover:bg-black hover:text-white hover:duration-300 ease-in-out">
-                                <a href="https://www.linkedin.com/in/uka-osim-9761601a0/">
-                                    Follow
-                                </a>
-                            </button>
-                        </div>
+
+                <div className="rounded-3xl bg-white p-2 w-[230px]">
+                    <div className="h-24 w-full rounded-3xl grid place-content-center bg-red-100">
+                        <FaLinkedin size={40} color="blue" />
+                    </div>
+                    <div className="flex flex-col items-center justify-between gap-[1rem] mt-[1rem]">
+                        <p className="font-bold text-2xl">
+                            132<span className="text-pink-500"></span>
+                        </p>
+                        <p>Linkedin Connections</p>
+                        <button className="rounded-3xl py-1 px-10 border-[1px] border-black font-bold hover:bg-black hover:text-white hover:duration-300 ease-in-out">
+                            <a href="https://www.linkedin.com/in/uka-osim-9761601a0/">
+                                Follow
+                            </a>
+                        </button>
                     </div>
                 </div>
+
             </div>
         </div>
     )
